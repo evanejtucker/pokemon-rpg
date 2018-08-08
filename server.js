e1df2371cb50315a8955618f7ac35b242d97f5d5
Evan Tucker <eej.tucker@gmail.com>
2018-08-08 17:22:12 -0600

    added file structure and express

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(express.static('public'))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// require('./routes/home-routes')(app);
+
+app.get('/', function (req, res) {
+    res.render('home');    
+});
+
+app.listen(port, (err)=> {
+    if (err) {
+        console.log('something went wrong');
+    } else {
+        console.log(`app listening on port ${port}!`);
+    }
+});
