e77770c942c410c281aa883ff551fb00250a2c11
Evan Tucker <eej.tucker@gmail.com>
2018-08-09 19:55:13 -0600

    fixed request route :smile:

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -1,21 +1,21 @@
 
 module.exports = (app, request)=> {
-    app.get('/api/all', (req, res, next)=> {
+    app.get('/api/pokemon/:id', (req, res, next)=> {
+        console.log(req.params);
         var pokemon;
-        // request('https://pokeapi.co/api/v2/pokemon/1/', function(err, response, body) {
-        //     console.log('it worked');
-        //     pokemon = body; 
-        // }).then(function() {
-        //     res.send(pokemon);
-        // });
+        var queryUrl = 'https://pokeapi.co/api/v2/pokemon/' + req.params.id + "/";
+        console.log(queryUrl);
+        console.log('waiting ...')
         request({
-            uri: 'https://pokeapi.co/api/v2/pokemon/1/',
-            method: 'GET'
+            uri: queryUrl
         }, function(error, response, body) {
             if (!error && response.statusCode === 200) {
-                console.log(body)
-              }
-        })
+                pokemon = JSON.parse(body);
+                console.log(pokemon.name);
+                res.json(pokemon); 
+                console.log('done');
+            }
+        });
     });
 };
 
