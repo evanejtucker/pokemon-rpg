1fb356b1e1fab1f1765073c53bd664677465472b
Evan Tucker <eej.tucker@gmail.com>
2018-08-10 12:27:44 -0600

    added some styles

--- public/scripts/pokemon.js
+++ public/scripts/pokemon.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
-    console.log('the script is running');
 
-    var pokemon = [];
+    $('#pokemonSubmit').on('click', function() {
+        event.preventDefault();
+        var name = $('#pokemonName').val();
+        console.log(name);
 
-    var getPokemon = function(number) {
-        var queryUrl = "https://pokeapi.co/api/v2/pokemon/" + number + "/";
-        console.log(queryUrl);
-        $.ajax({
-            method: 'get',
-            url: queryUrl
-        }).done(function(data) {
-            pokemon.push(data.name);
-        });
-        console.log(pokemon)
-    }
-
-    // getPokemon(1);
+        // $.get('/api/pokemon/' + name + '/', function(data) {
+        //     console.log(data)
+        //     console.log('worked');
+        // })
+    })
 
 });
