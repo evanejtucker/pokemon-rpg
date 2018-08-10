2b21e356515ecb7586a3812f9076b5573a7c38b8
Evan Tucker <eej.tucker@gmail.com>
2018-08-09 18:46:09 -0600

    worked on request

--- controller/api-routes.js
+++ controller/api-routes.js
@@ -1,11 +1,21 @@
 
 module.exports = (app, request)=> {
     app.get('/api/all', (req, res, next)=> {
-        var pokemon = ['Evan']
-        request('https://pokeapi.co/api/v2/pokemon/1/', function(err, response, body) {
-            pokemon.push("Evan");
-            res.send(response);
-        });        
+        var pokemon;
+        // request('https://pokeapi.co/api/v2/pokemon/1/', function(err, response, body) {
+        //     console.log('it worked');
+        //     pokemon = body; 
+        // }).then(function() {
+        //     res.send(pokemon);
+        // });
+        request({
+            uri: 'https://pokeapi.co/api/v2/pokemon/1/',
+            method: 'GET'
+        }, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body)
+              }
+        })
     });
 };
 
